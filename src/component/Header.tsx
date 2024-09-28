8a3e387702b88332c0efaa4344d2e90cd91911cc
maximdave <enoragbondavid35@gmail.com>
2024-09-28 09:25:29 +0100

    NSSF-17 | change: made header component reuseable

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,23 +1,16 @@
 import React, { FC } from "react";
-import wenaija_logo from "./Assets/wenaija_logo.svg";
-import settingsIcon from "./Assets/settingsIcon.svg";
-import notificationIcon from "./Assets/notificationIcon.svg";
-import profileIcon from "./Assets/profileIcon.svg";
-const Header: FC = () => {
+interface HeaderProps {
+  logo?: string;
+  children?: React.ReactNode;
+}
+
+const Header: FC<HeaderProps> = ({ logo, children }) => {
   return (
-    <header className="flex items-center justify-between p-2 bg-white shadow px-12">
-      <img src={wenaija_logo} alt="Wenaija Logo" className="h-6" />
-      <div className="flex ">
-        <button className=" text-white rounded">
-          <img src={settingsIcon} alt="settingsIcon" className="h-6" />
-        </button>
-        <button className="pr-2 text-white rounded">
-          <img src={notificationIcon} alt="notificationIcon" className="h-6" />
-        </button>
-        <button className=" text-white rounded">
-          <img src={profileIcon} alt="profileIcon" className="h-6" />
-        </button>
+    <header className="flex justify-between items-center shadow px-12 p-2">
+      <div className="flex items-center">
+        <img src={logo} alt=" Logo" className="h-6" />
       </div>
+      {children}
     </header>
   );
 };
