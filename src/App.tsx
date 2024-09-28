c0aae479913137cda63ad9012d62b8c944f95a48
maximdave <enoragbondavid35@gmail.com>
2024-09-28 09:49:17 +0100

    NSSF-17 | fix: removed header from app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Button from "./component/Button";
-import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button />
-        <Header />
       </header>
     </div>
   );
