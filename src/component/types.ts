7933f3a98e576479bfb58bf287bf89df70cef410
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-09-30 15:34:05 +0100

    NSSF-25

--- src/component/types.ts
+++ src/component/types.ts
@@ -1,9 +1,33 @@
 export interface ButtonProps {
-    bg_color?: string;
-    text?: string;
-    onClick?: () => void;
-    active?: boolean;
-    loading?: boolean;
-    text_color?: string;
-    border_color?: string;
+  bg_color?: string;
+  text?: string;
+  onClick?: () => void;
+  active?: boolean;
+  loading?: boolean;
+  text_color?: string;
+  border_color?: string;
+}
+
+export interface TextInputProps {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  helperText?: string;
+  placeHolder?: string;
+
+}
+
+export enum TypographyVariant {
+  TITLE,
+  SUBTITLE,
+  NORMAL,
+  BOLD,
+  SMALL,
+}
+
+export interface TypographyProps {
+  children: React.ReactNode;
+  variant: TypographyVariant;
+  className?: string;
 }
