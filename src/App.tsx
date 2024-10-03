35dd0ea9b02a1ff1ca3fb2dced5d504044314afa
maximdave <enoragbondavid35@gmail.com>
2024-10-03 04:27:53 +0100

    NSSF-23 | change: add close icon to modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import React, { useState } from "react";
-import Modal from "./component/Modal";
-import closeIcon from "./component/Assets/closeIcon.svg";
+import React from "react";
 
-const App = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-
+function App() {
   return (
-    <div>
-      <button
-        onClick={() => setModalOpen(true)}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Open Modal
-      </button>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setModalOpen(false)}
-        width="600px"
-        closeIcon={closeIcon} 
-      >
-        <h2 className="text-lg font-bold">My Modal</h2>
-        <p>This is the content of the modal!</p>
-        <p>This is the content of the modal!</p>
-        <p>This is the content of the modal!</p>
-        <p>This is the content of the modal!</p>
-        <p>This is the content of the modal!</p>
-      </Modal>
+    <div className="App">
+      <header className="header px-8">
+      </header>
     </div>
   );
-};
+}
 
 export default App;
