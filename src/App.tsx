9719d4217d871dbdf749e1bbdcad9c3cc96e0146
maximdave <enoragbondavid35@gmail.com>
2024-10-03 03:21:56 +0100

    NSSF-23 | add: Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./component/Modal";
 
-function App() {
+const App = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="header px-8">
-      </header>
+    <div>
+      <button
+        onClick={() => setModalOpen(true)}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Open Modal
+      </button>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        width="600px"
+      >
+        <h2 className="text-lg font-bold">My Modal</h2>
+        <p>This is the content of the modal!</p>
+        <p>This is the content of the modal!</p>
+        <p>This is the content of the modal!</p>
+        <p>This is the content of the modal!</p>
+        <p>This is the content of the modal!</p>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/Modal.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  width?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children, width }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div
+        className="bg-white rounded-lg shadow-lg p-6 "
+        style={{
+          width: width,
+        }}
+      >
+        {children}
+        <button
+          onClick={onClose}
+          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
