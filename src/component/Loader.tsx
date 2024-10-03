7dd4a7b79b0adf5191127a3bd8dd893085370535
maximdave <enoragbondavid35@gmail.com>
2024-10-03 05:17:32 +0100

    NSSF-27 | add: implement Loader component

---
+++ src/component/Loader.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import { LoaderProps } from "./types";
+
+const Loader: FC<LoaderProps> = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-gray-400 bg-opacity-50"
+      onClick={handleOutsideClick}
+    >
+      {/* Centering the loader */}
+      <div className="loader">
+        {" "}
+        {/* Loader container */}
+        <div className="loader-circle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
