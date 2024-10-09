cce22ba2e01188b04bed0272802b67a32fd3e4a3
maximdave <enoragbondavid35@gmail.com>
2024-10-09 23:49:53 +0100

    NSSF-18 | add: props description to header component

--- src/Pages/ViewHeader.tsx
+++ src/Pages/ViewHeader.tsx
@@ -5,14 +5,15 @@ import { TypographyVariant } from "../component/types";
 import logo from "../component/Assets/logo.svg";
 import { IoSettingsOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
+import PropsDescription from "../component/PropsDescription";
 
 const ViewHeader = () => {
   return (
-    <div>
+    <div className="pt-8">
       <Typography variant={TypographyVariant.TITLE} className="text-black">
         Header
       </Typography>
-      <div className="pt-6">
+      <div className="pt-8">
         <Header
           bg_color="pink"
           logo={logo}
@@ -31,6 +32,24 @@ const ViewHeader = () => {
             </>
           }
         />
+        <div className="mt-24">
+          <PropsDescription
+            rows={[
+              {
+                name: "bg_color",
+                description: "Background color of the header",
+              },
+              {
+                name: "logo",
+                description: "Logo of the header",
+              },
+              {
+                name: "children",
+                description: "Children of the header",
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
