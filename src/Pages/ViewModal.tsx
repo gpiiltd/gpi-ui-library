6a12cc20f91743bee237e4a94ee6716055d214ff
maximdave <enoragbondavid35@gmail.com>
2024-10-10 00:49:31 +0100

    NSSF-18 | add: props description to Modal component

--- src/Pages/ViewModal.tsx
+++ src/Pages/ViewModal.tsx
@@ -3,6 +3,7 @@ import Modal from "../component/Modal";
 import Typography from "../component/Typography";
 import { TypographyVariant } from "../component/types";
 import closeIcon from "../component/Assets/closeIcon.svg";
+import PropsDescription from "../component/PropsDescription";
 
 const ViewModal = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -36,6 +37,33 @@ const ViewModal = () => {
         <p>This is the content of the modal!</p>
         <p>This is the content of the modal!</p>
       </Modal>
+
+      <div className="mt-24">
+        <PropsDescription
+          rows={[
+            {
+              name: "isOpen",
+              description: "Open the modal",
+            },
+            {
+              name: "onClose",
+              description: "Close the modal",
+            },
+            {
+              name: "closeIcon",
+              description: "Close icon of the modal",
+            },
+            {
+              name: "width",
+              description: "Width of the modal",
+            },
+            {
+              name: "children",
+              description: "Children of the modal",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
