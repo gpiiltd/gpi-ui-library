27ce94e87d1216791bc31bcf8339e7d85a1e6cf0
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-10-11 14:41:33 +0100

    NSSF-34|feat:Footer story book

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FooterProps } from "../types";
 
-const Footer: React.FC<FooterProps> = ({ children, bg_color }) => {
+const Footer: React.FC<FooterProps> = ({ logo, children, bg_color }) => {
   return (
     <footer
       className={`bg-${bg_color} text-white py-4 bottom-0 absolute w-full_width`}
@@ -9,7 +9,11 @@ const Footer: React.FC<FooterProps> = ({ children, bg_color }) => {
         backgroundColor: bg_color,
       }}
     >
-      <div className="container mx-auto px-4 text-center">{children}</div>
+      <div className=" px-4 flex items-center gap-20">
+        <img src={logo} alt=" Logo" className="h-6" />
+
+        <div>{children}</div>
+      </div>
     </footer>
   );
 };
